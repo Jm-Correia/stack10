609978429cdc9d64de91e4a9cf3fb16b61b56a9d
Jm-Correia <joao.correia.marcos@gmail.com>
2020-01-29 21:30:41 -0300

    Comitando o Exemplo de CRUD utilizando  Rotas no express().

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,19 +1,42 @@
 const express = require('express');
 
-
-
 const app = express();
+app.use(express.json());
 
+/**
+ * Querys params = ?teste=1
+ * Router params = /users/1
+ * Request body = {"name": "Joao", "email": "1234565@gmail.com"} 
+ */
 
-app.get('/teste', (req, res) => {
-    const nome = req.query.nome;
-    return res.status(200).send(`Hello ${nome}`);
+const users = ['01','02','aspira', 'supremo', 'teste'];
+
+app.get('/users', (req, res)=>{
+    return res.json(users);
 });
-
 
 app.get('/users/:id', (req, res) => {
     const {id} = req.params;
-    return res.status(200).send(`Buscando o usuario ${id}`);
+    return res.status(200).send(`Buscando o usuario ${users[id]}`);
+});
+
+app.post('/users', (req, res)=>{
+    const { name } = req.body;
+    users.push(name);
+    return res.json(users);
+});
+
+app.put('/users/:id', (req, res)=>{
+    const {id} = req.params;
+    const {name} = req.body;
+    users[id] = name;
+    return res.json(users);
+});
+
+app.delete('/users/:id', (req, res)=>{
+    const {id} = req.params;
+    users.splice(id,1);
+    return res.send();
 });
 
 app.listen(3000);
