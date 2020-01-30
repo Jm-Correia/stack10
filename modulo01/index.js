5c4a8a721b59a82de55c08d91dbe0915a06939ce
Jm-Correia <joao.correia.marcos@gmail.com>
2020-01-29 21:47:27 -0300

    Add middlewares de exemplo global e especifo em rotas

--- modulo01/index.js
+++ modulo01/index.js
@@ -4,6 +4,32 @@ const app = express();
 app.use(express.json());
 
 /**
+ * Middlewares GLOBAL
+ * ################## EXEMPLO ###########################
+*/
+app.use((req, res, next)=>{
+    console.log(`Método ${req.method}, URL ${req.url}`);
+    return next();
+});
+/**
+ * Midlewares LOCAL
+ * ################## EXEMPLO ###########################
+ */
+function checkUserExists(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({err: 'User name is required'});
+    }
+    return next();
+}
+
+function checkUserArray(req, res, next){
+    if(!users[req.params.id]){
+        return res.status(400).json({err: 'User doesn\'t exists'});
+    }
+    return next();
+}
+
+/**
  * Querys params = ?teste=1
  * Router params = /users/1
  * Request body = {"name": "Joao", "email": "1234565@gmail.com"} 
@@ -20,20 +46,21 @@ app.get('/users/:id', (req, res) => {
     return res.status(200).send(`Buscando o usuario ${users[id]}`);
 });
 
-app.post('/users', (req, res)=>{
+app.post('/users', checkUserExists, (req, res)=>{
     const { name } = req.body;
+    
     users.push(name);
     return res.json(users);
 });
 
-app.put('/users/:id', (req, res)=>{
+app.put('/users/:id', checkUserExists, (req, res)=>{
     const {id} = req.params;
     const {name} = req.body;
     users[id] = name;
     return res.json(users);
 });
 
-app.delete('/users/:id', (req, res)=>{
+app.delete('/users/:id', checkUserArray, (req, res)=>{
     const {id} = req.params;
     users.splice(id,1);
     return res.send();
