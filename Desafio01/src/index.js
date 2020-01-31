8c722995bb1d1b9d7d6bbf4f793a62a0c4e6be55
Jm-Correia <joao.correia.marcos@gmail.com>
2020-01-30 21:12:53 -0300

    Add listar projetos e cadastrar projetos

--- Desafio01/src/index.js
+++ Desafio01/src/index.js
@@ -4,10 +4,36 @@ const app = express();
 
 app.use(express.json());
 
-const tasks = [];
+const projects = [];
 
+app.get('/projects', (req, res)=>{
+    return res.json(projects);
+});
 
+app.post('/projects', (req, res)=>{
+    const {id, title} = req.body;
+    
+    let project = {
+        "id": id,
+        "title": title,
+        "tasks": []
+    }
+    
+    projects.push(project);
 
+    return res.json(projects);
+});
 
+app.put('/projetcs/:id', (req, res)=>{
+
+});
+
+app.delete('/projects/:id', (req, res) =>{
+
+});
+
+app.get('/projects/:id/tasks', (req, res)=>{
+
+});
 
 app.listen(3000);
