6b1ca403b30486b4625954f28ef0374ccd5de2c1
Jm-Correia <joao.correia.marcos@gmail.com>
2020-02-09 16:21:52 -0300

    Add Column avatar_id inside users, associate File.id how foreign key. Create model File.

--- gobarber/src/database/index.js
+++ gobarber/src/database/index.js
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 
 import dataBaseConfig from '../config/databases';
 import User from '../app/models/User';
+import File from '../app/models/File';
 
-const models = [User];
+const models = [User, File];
 
 class DataBase {
     constructor() {
@@ -13,7 +14,12 @@ class DataBase {
     init() {
         this.connection = new Sequelize(dataBaseConfig);
 
-        models.map(model => model.init(this.connection));
+        models
+            .map(model => model.init(this.connection))
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
--- gobarber/src/router.js
+++ gobarber/src/router.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
 import authConfig from './app/middleware/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
@@ -11,5 +17,7 @@ routes.post('/session', SessionController.store);
 routes.use(authConfig);
 
 routes.put('/users', UserController.update);
+
+routes.post('/file', upload.single('file'), FileController.store);
 
 export default routes;
