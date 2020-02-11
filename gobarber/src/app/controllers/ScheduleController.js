02414eb4e829f2f80fcec66a0272c7a61d18bbfd
Jm-Correia <joao.correia.marcos@gmail.com>
2020-02-10 21:58:10 -0300

    Add ScheduleController

---
+++ gobarber/src/app/controllers/ScheduleController.js
@@ -0,0 +1,35 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class ScheduleController {
+    async index(req, res) {
+        const checkUserProvider = await User.findOne({
+            where: { id: req.userId, provider: true },
+        });
+
+        if (!checkUserProvider) {
+            return res.status(401).json({
+                error: 'User is not a provider',
+            });
+        }
+
+        const { data } = req.query;
+        const parseDate = parseISO(data);
+
+        const appointments = await Appointment.findAll({
+            where: {
+                provider_id: req.userId,
+                canceled_at: null,
+                data: {
+                    [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+                },
+            },
+        });
+
+        return res.json(appointments);
+    }
+}
+
+export default new ScheduleController();
--- gobarber/src/router.js
+++ gobarber/src/router.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authConfig from './app/middleware/auth';
 import multerConfig from './config/multer';
@@ -21,8 +22,11 @@ routes.use(authConfig);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
-routes.post('/appointments', AppointmentController.store);
+
 routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/file', upload.single('file'), FileController.store);
 
