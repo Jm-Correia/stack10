d2f4b0b44940ec2f8b4b492f921fe4f569bb750f
Jm-Correia <joao.correia.marcos@gmail.com>
2020-03-01 13:43:02 -0300

    Add nodemailer and Function of canceled appointment with send email to provider

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,9 +1,10 @@
 import * as YUP from 'yup';
-import { parseISO, startOfHour, isBefore, format } from 'date-fns';
+import { parseISO, startOfHour, isBefore, format, subHours } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
     async index(req, res) {
@@ -116,6 +117,41 @@ class AppointmentController {
         });
 
         return res.json(appointments);
+    }
+
+    async delete(req, res) {
+        const appointment = await Appointment.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    as: 'provider',
+                    attributes: ['name', 'email'],
+                },
+            ],
+        });
+
+        if (appointment.user_id !== req.userId) {
+            return res.status(401).json({
+                error: "You don't have permission to cancel this appointment",
+            });
+        }
+
+        const dateWithSub = subHours(appointment.data, 2);
+
+        if (isBefore(dateWithSub, new Date())) {
+            return res.status(401).json({
+                error: 'You can only cancel appointments 2 hours in advence.',
+            });
+        }
+        await Mail.sendMail({
+            to: `${appointment.provider.name} <${appointment.provider.email}>`,
+            subject: 'Agendamento Cancelado',
+            text: 'Vc tem um novo cancelamento',
+        });
+
+        appointment.canceled_at = new Date();
+        await appointment.save();
+        return res.json({ appointment });
     }
 }
 
---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,18 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '5dd851cea3c980',
+        pass: '113dead4c088f8',
+    },
+    default: {
+        from: 'Team gobarber <noreply@gobarber.com>',
+    },
+};
+
+/**
+ * AMAZON SES
+ * MAILGUN
+ * SPARKPOST
+ */
