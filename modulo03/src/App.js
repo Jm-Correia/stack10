d253e01c584c209bdf6c4ae4be44c0baa2dd83a7
Jm-Correia <joao.correia.marcos@gmail.com>
2020-03-07 16:48:36 -0300

    Example the configuration for workspace reactJS use babel and webpack

---
+++ modulo03/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import profile from './imagens/profile.jpg';
+
+function App(){
+    return <img width='400' src={profile}/>
+}
+
+export default App;
