647a300a479bd8322bb39a6f2b163a2d035d0fbd
João Correia <joao.correia.marcos@gmail.com>
2020-08-09 15:29:23 -0300

    add comunicação com api do git e detalhando.

--- TypeScript/ReactJS/git-repo/src/pages/Repository/index.tsx
+++ TypeScript/ReactJS/git-repo/src/pages/Repository/index.tsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useRouteMatch, Link} from 'react-router-dom';
 import {FiChevronLeft, FiChevronRight} from 'react-icons/fi';
+import api from '../../services/apis';
 
 import logoImg from '../../assets/logo.svg';
-import {Header, Info, Issues} from './styles'
+import {Header, Info, Issues} from './styles';
 
 interface RepositoryParams{
     repository: string;
 }
 
+
+interface Repository{
+    full_name: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+    description:string;
+    stargazers_count: number;
+    open_issues_count: number;
+    forks_count:number;
+
+}
+
+interface Issue{
+    id: number;
+    title: string;
+    user:{
+        login: string;
+    };
+    html_url: string
+}
+
 const Repository:React.FC = () => {
     const {params} = useRouteMatch<RepositoryParams>();
+
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState<Issue[]>([]);
+   
+    useEffect(()=>{
+        api.get(`repos/${params.repository}`).then(response =>{
+            setRepository(response.data)
+        })
+
+        api.get(`repos/${params.repository}/issues`).then(response =>{
+            setIssues(response.data)
+        });
+
+
+    },[params.repository]);
     return (
         <>
         <Header>
             <img src={logoImg} alt="GitHub explorer"/>
-            <Link to="/dashboard">
+            <Link to="">
                <div>
                     <FiChevronLeft size={16}/>
                     <strong>Voltar</strong>
@@ -24,38 +63,40 @@ const Repository:React.FC = () => {
         </Header>
         <Info>
             <header>
-                <img src={logoImg}></img>
+                <img src={repository?.owner.avatar_url}></img>
                 <div>
-                    <strong>Repo/repo</strong>
-                    <p>descricao</p>
+                    <strong>{repository?.full_name}</strong>
+                    <p>{repository?.description}</p>
                 </div>
             </header>
             <ul>
                 <li>
-                    <strong>1808</strong>
+                    <strong>{repository?.stargazers_count}</strong>
                     <span>Stars</span>
                 </li>
                 <li>
-                    <strong>47</strong>
+                    <strong>{repository?.forks_count}</strong>
                     <span>Forks</span>
                 </li>
                 <li>
-                    <strong>67</strong>
+                    <strong>{repository?.open_issues_count}</strong>
                     <span>Issues Abertas</span>
                 </li>
             </ul>
         </Info>
         <Issues>
-        
-            <Link to="">
-                <img src="" alt=""/>
-                <div >
-                    <strong>{}</strong>
-                    <p>{}</p>
-                </div>
-            
-                <FiChevronRight size={20}/>
-            </Link>
+            {issues.map(issue => (
+                <a key={issue.id} href={`${issue.html_url}`}>
+                    
+                    <div >
+                        <strong>{issue.title}</strong>
+                        <p>{issue.user.login}</p>
+                    </div>
+                
+                    <FiChevronRight size={20}/>
+                </a>
+
+            ))}
         
         </Issues>
         </>
